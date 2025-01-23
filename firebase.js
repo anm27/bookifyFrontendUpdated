b827e3e9bba6f89b962e1fe88cab0c3751534f50
anm27 <94228648+anm27@users.noreply.github.com>
2025-01-24 01:16:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,14 @@ import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvpuCQskOZZTHiK3p8IDjioBjSYyPYZQ8",
-  authDomain: "maid-security-e7d05.firebaseapp.com",
-  projectId: "maid-security-e7d05",
-  storageBucket: "maid-security-e7d05.appspot.com",
-  messagingSenderId: "624698810794",
-  appId: "1:624698810794:web:5b818fe295968a6a9c8bd3",
-  measurementId: "G-NT0HPXFZ82",
-  databaseUrl: "https://maid-security-e7d05-default-rtdb.firebaseio.com/",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+  databaseUrl: "",
 };
 
 // Initialize Firebase
